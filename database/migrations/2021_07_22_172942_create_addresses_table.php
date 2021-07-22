e5a0b7a87d7cf3d89d8253dae43b5c248f05f097
Nilson Junior <nilsonjrsilva1997@gmail.com>
2021-07-22 10:42:31 -0700

    criando projeto e migrations

---
+++ database/migrations/2021_07_22_172942_create_addresses_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAddressesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('cep');
+            $table->string('rua');
+            $table->string('cidade');
+            $table->string('bairro');
+            $table->string('estado');
+            $table->integer('numero');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('addresses');
+    }
+}
